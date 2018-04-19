15be535d0528a5419f955afbf1ea417aa898b395
oboehm <o.boehm@optica.de>
2018-04-19 08:57:38 +0200

    first test for IppRequestHandler added

---
+++ src/test/java/j4cups/server/IppRequestHandlerTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018 by Oliver Boehm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (c)reated 19.04.2018 by oboehm (ob@oasd.de)
+ */
+package j4cups.server;
+
+import org.apache.http.*;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
+import org.apache.http.protocol.HttpContext;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Unit tests for {@link IppRequestHandler}.
+ */
+class IppRequestHandlerTest {
+    
+    private final IppRequestHandler requestHandler = new IppRequestHandler();
+
+    /**
+     * For a first simple test we just call the handle method.
+     * 
+     * @throws IOException in case of network problems
+     * @throws HttpException HTTP exception
+     */
+    @Test
+    void testHandle() throws IOException, HttpException {
+        HttpPost request = createHttpRequest();
+        HttpResponse response = createHttpResponse();
+        HttpContext context = new HttpClientContext();
+        requestHandler.handle(request, response, context);
+    }
+
+    private static HttpPost createHttpRequest() throws UnsupportedEncodingException {
+        HttpPost request = new HttpPost();
+        HttpEntity entity = new StringEntity("hello");
+        request.setEntity(entity);
+        return request;
+    }
+
+    private static HttpResponse createHttpResponse() {
+        ProtocolVersion protocolVersion = new ProtocolVersion("HTTP", 1, 0);
+        StatusLine statusLine = new BasicStatusLine(protocolVersion, 200, "OK");
+        return new BasicHttpResponse(statusLine);
+    }
+
+}
