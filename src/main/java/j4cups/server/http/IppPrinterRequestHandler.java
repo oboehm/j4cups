441b88ff1c9422d1f2d90af91978d1b50434e4b9
Oli B <ob@jugs.org>
2018-06-03 14:39:44 +0200

    requests are recorded in /tmp/IPP/printer directory

--- src/main/java/j4cups/server/http/IppPrinterRequestHandler.java
+++ src/main/java/j4cups/server/http/IppPrinterRequestHandler.java
@@ -22,10 +22,14 @@ import j4cups.op.GetPrinterAttributes;
 import j4cups.op.Operation;
 import j4cups.protocol.IppRequest;
 import j4cups.protocol.IppResponse;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * The IppPrinterRequestHandler emulates an IPP printer and handles the
@@ -37,6 +41,24 @@ import org.slf4j.LoggerFactory;
 public final class IppPrinterRequestHandler extends AbstractIppRequestHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(IppPrinterRequestHandler.class);
+    private final Path recordDir;
+
+    /**
+     * Instantiates the request handler for the printer emulation. The requests
+     * will be recorded to "/tmp/IPP/printer" (on Unix).
+     */
+    public IppPrinterRequestHandler() {
+        this(Paths.get(SystemUtils.getJavaIoTmpDir().toString(), "IPP", "printer"));
+    }
+
+    /**
+     * Instantiates the request handler for the printer emulation.
+     *
+     * @param recordDir directory, where the requests are logged
+     */
+    public IppPrinterRequestHandler(Path recordDir) {
+        this.recordDir = recordDir;
+    }
 
     /**
      * Handles the request and produces a response to be sent back to
@@ -49,6 +71,7 @@ public final class IppPrinterRequestHandler extends AbstractIppRequestHandler {
     public void handle(HttpEntityEnclosingRequest request, HttpResponse response) {
         IppRequest ippRequest = IppEntity.toIppRequest(request);
         LOG.info("{} received.", ippRequest.toShortString());
+        ippRequest.recordTo(recordDir);
         IppResponse ippResponse = new IppResponse(ippRequest);
         switch (ippRequest.getOperation()) {
             case GET_PRINTER_ATTRIBUTES:
@@ -58,6 +81,7 @@ public final class IppPrinterRequestHandler extends AbstractIppRequestHandler {
         }
         IppEntity ippEntity = new IppEntity(ippResponse);
         response.setEntity(ippEntity);
+        ippResponse.recordTo(recordDir);
         LOG.info("Response {} is filled.", response);
     }
 
