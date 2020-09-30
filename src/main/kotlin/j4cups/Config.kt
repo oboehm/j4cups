fae1ab2b3ef016c5441fa800d510ac91e7cb29c0
Oli B <o.boehm@optica.de>
2020-09-30 19:27:06 +0200

    Config migrated to Kotlin

--- src/main/kotlin/j4cups/Config.kt
+++ src/main/kotlin/j4cups/Config.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 by Oliver Boehm
+ * Copyright (c) 2018-2020 by Oliver Boehm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,59 +15,24 @@
  *
  * (c)reated 13.12.2018 by oboehm (ob@oasd.de)
  */
-package j4cups;
+package j4cups
 
-import j4cups.util.XProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.util.Properties;
+import j4cups.util.XProperties
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.IOException
+import java.net.URI
+import java.util.*
 
 /**
- * Configuration stuff for J4Cups
+ * Configuration stuff for J4Cups.
  *
  * @author oboehm
  * @since 0.6 (13.12.2018)
  */
-public final class Config {
-    
-    private static final Logger LOG = LoggerFactory.getLogger(Config.class);
+class Config @JvmOverloads constructor(resource: String = "j4cups/standalone.properties") {
 
-    private final Properties properties;
-
-    /**
-     * Creates a new configuration for a standalone server.
-     */
-    public Config() {
-        this("j4cups/standalone.properties");
-    }
-    
-    /**
-     * Instantiates a new Configuration with the given resource
-     *
-     * @param resource the resource, e.g. "j4cups/proxy.properties"
-     */
-    public Config(String resource) {
-        this.properties = loadProperties(resource);
-    }
-
-    private static Properties loadProperties(String resource) {
-        Properties properties = new XProperties();
-        try (InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource)) {
-            if (input == null) {
-                throw new IllegalArgumentException("resource '" + resource + "' is not in classpath");
-            }
-            properties.load(input);
-            LOG.info("Properties are loaded from '{}'.", resource);
-            return properties;
-        } catch (IOException ex) {
-            throw new IllegalArgumentException("cannot read init parameters from resource " + resource, ex);
-        }
-    }
+    private val properties: Properties
 
     /**
      * Fluent API for setting a property.
@@ -76,9 +41,9 @@ public final class Config {
      * @param value the value
      * @return the config
      */
-    public Config withProperty(String key, String value) {
-        this.properties.setProperty(key, value);
-        return this;
+    fun withProperty(key: String, value: String): Config {
+        properties.setProperty(key, value)
+        return this
     }
 
     /**
@@ -87,8 +52,8 @@ public final class Config {
      * @param port the port
      * @return the config
      */
-    public Config withServerPort(int port) {
-        return this.withProperty("j4cups.server.port", Integer.toString(port));
+    fun withServerPort(port: Int): Config {
+        return withProperty("j4cups.server.port", Integer.toString(port))
     }
 
     /**
@@ -96,9 +61,8 @@ public final class Config {
      *
      * @return the server port
      */
-    public int getServerPort() {
-        return getIntProperty("j4cups.server.port");
-    }
+    val serverPort: Int
+        get() = getIntProperty("j4cups.server.port")
 
     /**
      * If the server acts like a proxy you set the URI to the forwardded
@@ -107,8 +71,8 @@ public final class Config {
      * @param uri the URI where requests are forwarded
      * @return the config
      */
-    public Config withServerForwardURI(String uri) {
-        return this.withProperty("j4cups.server.forwardURI", uri);
+    fun withServerForwardURI(uri: String): Config {
+        return withProperty("j4cups.server.forwardURI", uri)
     }
 
     /**
@@ -116,21 +80,41 @@ public final class Config {
      *
      * @return the server forward uri
      */
-    public URI getServerForwardURI() {
-        String uri = this.properties.getProperty("j4cups.server.forwardURI");
-        if (uri == null) {
-            return new File(this.properties.getProperty("j4cups.server.recordDIR")).toURI();
-        } else {
-            return URI.create(uri);
+    val serverForwardURI: URI
+        get() {
+            val uri = properties.getProperty("j4cups.server.forwardURI")
+            return if (uri == null) {
+                File(properties.getProperty("j4cups.server.recordDIR")).toURI()
+            } else {
+                URI.create(uri)
+            }
+        }
+    val serverInfo: String
+        get() = properties.getProperty("j4cups.server.info")
+
+    private fun getIntProperty(key: String): Int {
+        return properties.getProperty(key).toInt()
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(Config::class.java)
+        private fun loadProperties(resource: String): Properties {
+            val properties: Properties = XProperties()
+            try {
+                Thread.currentThread().contextClassLoader.getResourceAsStream(resource).use { input ->
+                    requireNotNull(input) { "resource '$resource' is not in classpath" }
+                    properties.load(input)
+                    LOG.info("Properties are loaded from '{}'.", resource)
+                    return properties
+                }
+            } catch (ex: IOException) {
+                throw IllegalArgumentException("cannot read init parameters from resource $resource", ex)
+            }
         }
     }
 
-    public String getServerInfo() {
-        return this.properties.getProperty("j4cups.server.info");
+    init {
+        properties = loadProperties(resource)
     }
 
-    private int getIntProperty(String key) {
-        return Integer.parseInt(this.properties.getProperty(key));
-    }
-    
 }
